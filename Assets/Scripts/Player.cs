bfc6066de22b68d9ea61e4a3fab59633d25fffc3
Fsly <2645358281@qq.com>
2019-07-03 00:00:55 +0800

    move11

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,8 +18,8 @@ public class Player : MonoBehaviour
 
     private float restTimer = 0;
     [HideInInspector] public Vector2 targetPos = new Vector2(1, 1);
-    private Rigidbody2D rigidbody;
-    private BoxCollider2D collider;
+    private new Rigidbody2D rigidbody;
+    private new BoxCollider2D collider;
     private Animator animator;
     // Use this for initialization
     void Start()
@@ -32,14 +32,17 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //使用刚体控制移动
         rigidbody.MovePosition(Vector2.Lerp(transform.position, targetPos, smoothing * Time.deltaTime));
 
+        //游戏结束无法移动
         if (GameManager.Instance.food <= 0 || GameManager.Instance.isEnd == true) return;
 
+        //休息计时器
         restTimer += Time.deltaTime;
         if (restTimer < restTime) return;
 
+        //获取输入
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
         if (h > 0)
@@ -50,10 +53,12 @@ public class Player : MonoBehaviour
         if (h != 0 || v != 0)
         {
             GameManager.Instance.ReduceFood(1);
-            //检测
+
+            //检测，启用行为会更新，禁用行为不会更新。
             collider.enabled = false;
             RaycastHit2D hit = Physics2D.Linecast(targetPos, targetPos + new Vector2(h, v));
             collider.enabled = true;
+            //空地
             if (hit.transform == null)
             {
                 targetPos += new Vector2(h, v);
